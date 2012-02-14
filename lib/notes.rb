7f11e44afc4a81e36eeb6e9419b534b728baf13f
Vivien Didelot <vivien@didelot.org>
2012-02-14 03:35:01 -0500

    remove license text from notes.rb

--- lib/notes.rb
+++ lib/notes.rb
@@ -1,10 +1,3 @@
-# ------------------------------------------------------------------------------
-# "THE BEER-WARE LICENSE" (Revision 42):
-# <vivien.didelot@gmail.com> wrote this file. As long as you retain this notice
-# you can do whatever you want with this stuff. If we meet some day, and you
-# think this stuff is worth it, you can buy me a beer in return. Vivien Didelot
-# ------------------------------------------------------------------------------
-
 require 'notes/version'
 require 'notes/scanner'
 
